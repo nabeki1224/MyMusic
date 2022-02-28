7544d5a852efa92dd123f48b39dc5def11147696
EC2 Default User <ec2-user@ip-172-31-29-161.ap-northeast-1.compute.internal>
2022-02-28 16:01:05 +0000

    Fixed bug on mobile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "05178a72ef6e4e689b893c2e08f48927.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
